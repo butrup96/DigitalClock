db5a0c1b2a2c5814264c727fffc45c648edb63e2
Brandon Utrup <47999059+butrup96@users.noreply.github.com>
2021-03-29 18:27:54 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHour.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHour.ForeColor = System.Drawing.Color.White;
+            this.lblHour.Location = new System.Drawing.Point(89, 81);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(285, 130);
+            this.lblHour.TabIndex = 0;
+            this.lblHour.Text = "22:22";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSecond.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSecond.ForeColor = System.Drawing.Color.White;
+            this.lblSecond.Location = new System.Drawing.Point(378, 123);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(82, 67);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "22";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDay.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDay.ForeColor = System.Drawing.Color.White;
+            this.lblDay.Location = new System.Drawing.Point(496, 301);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(203, 67);
+            this.lblDay.TabIndex = 2;
+            this.lblDay.Text = "Monday";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(89, 301);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(345, 67);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "March 29 2021";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblHour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
